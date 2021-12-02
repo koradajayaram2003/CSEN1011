9cf9093aef314d89306193f7847c2b219c3d70a3
koradajayaram2003 <93367843+koradajayaram2003@users.noreply.github.com>
2021-12-02 14:49:52 +0530

    Update markspercentage.c

--- markspercentage.c
+++ markspercentage.c
@@ -1,3 +1,18 @@
+/*Algorithm
+Step 1 :Start
+Step 2 :Enter the marks of a
+Step 3 :Enter the marks of b
+Step 4 :Enter the marks of c
+Step 5 :Enter the marks of d
+Step 6 :Enter the marks of e
+Step 7 :caculate the average marks and percentage
+Step 8 :Stop
+/*
+
+
+
+
+
 #include<stdio.h>
 int main()
 {
@@ -14,5 +29,10 @@ int main()
 	scanf("%d", &e);
 	printf("Average marks=%d", (a+b+c+d+e/5));
 }
+
+
+/*learnings
+...............to calculate average marks
+...............to calculate percentage of marks
 
 
