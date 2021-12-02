90ed18bece60c72be084da0057839da8c8366b17
koradajayaram2003 <93367843+koradajayaram2003@users.noreply.github.com>
2021-12-02 14:22:12 +0530

    Add files via upload

---
+++ markspercentage.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e;
+	printf("marks of a=");
+	scanf("%d", &a);
+	printf("marks of b =");
+	scanf("%d", &b);
+	printf("marks of c=");
+	scanf("%d", &c);
+	printf("marks of d=");
+	scanf("%d", &d);
+	printf("marks of e=");
+	scanf("%d", &e);
+	printf("Average marks=%d", (a+b+c+d+e/5));
+}
+
+
