a0db89148a42c0ab4647f3b34984ab5fa0eeec60
koradajayaram2003 <93367843+koradajayaram2003@users.noreply.github.com>
2021-12-31 11:34:47 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,14 @@
+#include <stdio . h>
+int main() {
+  int numbeer ;
+
+  printf("Enter an integer: ") ;
+
+  // read and store input
+  scanf("%d" , &number) ;
+
+  // displays output
+  printf("you entered: %d , number")
+  
+  return 0 ;
+}
